99b16de2a47fe5f021d8d97723864beda5e319f4
Bee - Berenice Lauren Pirlot <bl.pirlot@interface3.be>
2020-06-26 14:58:28 +0200

    final

---
+++ TeleporterGun/Assets/Scripts/FireProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectile : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+   
+
+    private void fire()
+    {
+        Vector3 position = spawnPoint.position;
+        Quaternion rotation = spawnPoint.rotation;
+
+        Instantiate(projectile, position, rotation);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fire();
+        }
+
+
+    }
+}
